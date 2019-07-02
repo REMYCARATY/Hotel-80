0ac3a426b2c41edfc77619d1eea1e3c5f4bf272f
REMYCARATY <43670928+REMYCARATY@users.noreply.github.com>
2019-07-02 08:40:20 +0200

    Add files via upload

---
+++ Site Hotel80/asset/js/main.js
@@ -0,0 +1,5 @@
+var myBtn = document.getElementById('btn concept');
+
+//add event listener
+myBtn.addEventListener('onclick', function(event) {window.location.href='C:/Users/Remy/Desktop/Hotel-80/Site Hotel80/pages/concept.html';
+});
